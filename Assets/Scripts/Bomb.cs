c74abd934eeef18b12d25d18c41f1656450c7faa
Maxim Kruk <maximkr@playtika.com>
2020-10-21 12:13:16 +0300

    Bomberman

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float timer = 3.0f;
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(this);
+        var v = new Vector2(transform.position.x, transform.position.y);
+        var colliders = Physics2D.OverlapCircleAll(v, 2.0f);
+            foreach (var c in colliders)
+            {
+                if(c.CompareTag("Dist"))
+                    Destroy(c.gameObject);
+                if(c.CompareTag("Player"))
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private LayerMask raycastMask;
+    [SerializeField] private LayerMask raycastMaskExplosion;
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private GameObject placeForBomb;
+    private bool isInMovement;
+    
+    void Update()
+    {
+        if (isInMovement)
+            return;
+        if (Input.GetKeyDown(KeyCode.A))
+            MovePlayerTo(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.D))
+            MovePlayerTo(Vector2.right);
+        if (Input.GetKeyDown(KeyCode.W))
+            MovePlayerTo(Vector2.up);
+        if (Input.GetKeyDown(KeyCode.S))
+            MovePlayerTo(Vector2.down);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantBomb();
+        }
+    }
+
+    private void MovePlayerTo(Vector2 dir)
+    {
+        if (Raycast(dir))
+            return;
+        isInMovement = true;
+
+        var pos = (Vector2) transform.position + dir;
+        transform.DOMove(pos, 0.5f).OnComplete(() =>
+        {
+            isInMovement = false;
+        });
+    }
+
+    private bool Raycast(Vector2 dir)
+    {    
+        var hit = Physics2D.Raycast(transform.position, dir, 1f, raycastMask);
+        return hit.collider != null;
+    }
+
+    //private GameObject RaycastFromCamera()
+    //{
+    //    var hit = Physics2D.Raycast(
+    //        Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 1f);
+    //    return hit.collider ? hit.collider.gameObject : null;
+    //}
+
+    private void PlantBomb()
+    {
+        var b = Instantiate(bomb, placeForBomb.transform.parent, true);
+        b.transform.position = transform.position;
+    }
+}
